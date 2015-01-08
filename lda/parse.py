c1f70ab38650f561bf9e0ea103c09baf4ce459d6
Çağıl Uluşahin <cagilulusahin@gmail.com>
2015-01-08 17:54:01 +0200

    to ldac

--- lda/parse.py
+++ lda/parse.py
@@ -7,6 +7,8 @@ punkt_word_tokenizer = PunktWordTokenizer()
 
 STOPWORDS = stopwords.words('turkish')
 VOCABULARY = {}
+VOCABULARY_l = []
+LDAC= []
 
 def main():
     wikifile = codecs.open("allDocsTogether","rU","utf-8")
@@ -18,7 +20,6 @@ def main():
 	body = wikifile.readline()
 	while body != u'\n':
 		add_to_vocabulary_text(body)
-		print(body)
 		body = wikifile.readline()
     wikifile.close()
 
@@ -38,3 +39,42 @@ def add_to_vocabulary(word):
         VOCABULARY[word] = VOCABULARY[word]+1
     else:
         VOCABULARY[word] = 1
+
+def to_ldac():
+    VOCABULARY_l = VOCABULARY.keys()
+    docs = []
+    wikifile = codecs.open("allDocsTogether","rU","utf-8")
+    vocabulary = {}
+    for i in range(0,5):
+	title = wikifile.readline()
+	print("Title: %s" %title)
+	wikifile.readline()
+	body = wikifile.readline()
+	while body != u'\n':
+		from_par_to_ldac(body)
+		body = wikifile.readline()
+    wikifile.close()
+
+def from_par_to_ldac(text):
+    sentences = tokenizer.tokenize(text)
+    for sent in sentences:
+        words = punkt_word_tokenizer.tokenize(sent)
+        from_sen_to_ldac(words)
+
+
+def from_sen_to_ldac(words):
+    temp = {}
+    for word in words:
+            try:
+                index = VOCABULARY_l.index(word)
+                if temp.has_key(index):
+                    temp[index] = temp[index] + 1
+                else:
+                    temp[index] = 1
+            except ValueError:
+                pass
+    line = ["[ %d " % len(temp),]
+    for word,freq in temp:
+        line.append("%s:%s " %(word,freq))
+    line.append("]")
+    print(line)
