0db856e88f14e61e6e4ed29d353034f68407bf11
Çağıl Uluşahin <cagilulusahin@gmail.com>
2015-01-08 19:28:58 +0200

    pickle, tokenizer and various

--- lda/parse.py
+++ lda/parse.py
@@ -1,10 +1,14 @@
-import codecs
+import codecs,pickle
 from nltk.corpus import stopwords
 import nltk.data
-from nltk.tokenize import PunktWordTokenizer
-tokenizer = nltk.data.load('tokenizers/punkt/turkish.pickle')
-punkt_word_tokenizer = PunktWordTokenizer()
 
+tokenizer = nltk.data.load('tokenizers/punkt/turkish.pickle')
+from nltk.tokenize import word_tokenize
+# alternative word tokenizer
+#from nltk.tokenize import PunktWordTokenizer
+#punkt_word_tokenizer = PunktWordTokenizer()
+
+COUNTER = 5
 STOPWORDS = stopwords.words('turkish')
 VOCABULARY = {}
 VOCABULARY_l = []
@@ -12,27 +16,29 @@ LDAC= []
 
 def main():
     wikifile = codecs.open("allDocsTogether","rU","utf-8")
-    vocabulary = {}
-    for i in range(0,5):
-	title = wikifile.readline()
-	print("Title: %s" %title)
-	wikifile.readline()
-	body = wikifile.readline()
-	while body != u'\n':
-		add_to_vocabulary_text(body)
-		body = wikifile.readline()
+    for _ in range(0,COUNTER):
+        title = wikifile.readline()
+        print("Title: %s" %title)
+        wikifile.readline()
+        body = wikifile.readline()
+        while body != u'\n':
+            add_to_vocabulary_text(body)
+            body = wikifile.readline()
+    out = open("voc1.pickle","wb")
+    pickle.dump(VOCABULARY,out)
+    out.close()
     wikifile.close()
 
 def add_to_vocabulary_text(text):
     sentences = tokenizer.tokenize(text)
     for sent in sentences:
-        words = punkt_word_tokenizer.tokenize(sent)
+        words = word_tokenize(sent)
         for word in words:
             add_to_vocabulary(word)
 
 def add_to_vocabulary(word):
     word =  word.lower()
-    if word in STOPWORDS or not word.isalnum():
+    if word in STOPWORDS or word in tokenizer.PUNCTUATION or not word.isalnum():
         print(word)
         return
     if VOCABULARY.has_key(word):
@@ -42,39 +48,39 @@ def add_to_vocabulary(word):
 
 def to_ldac():
     voc = VOCABULARY.keys()
-    docs = []
     wikifile = codecs.open("allDocsTogether","rU","utf-8")
-    vocabulary = {}
-    for i in range(0,5):
-	title = wikifile.readline()
-	print("Title: %s" %title)
-	wikifile.readline()
-	body = wikifile.readline()
-	while body != u'\n':
-		from_par_to_ldac(body,voc)
-		body = wikifile.readline()
+    for _ in range(0,COUNTER):
+        title = wikifile.readline()
+        print("Title: %s" %title)
+        wikifile.readline()
+        body = wikifile.readline()
+        while body != u'\n':
+            from_par_to_ldac(body,voc)
+            body = wikifile.readline()
     wikifile.close()
+    out = open("corpus1.pickle","wb")
+    pickle.dump(VOCABULARY,out)
+    out.close()
 
 def from_par_to_ldac(text,voc):
     sentences = tokenizer.tokenize(text)
     for sent in sentences:
-        words = punkt_word_tokenizer.tokenize(sent)
+        words = word_tokenize(sent)
         from_sen_to_ldac(words,voc)
-
 
 def from_sen_to_ldac(words,voc):
     temp = {}
     for word in words:
-            try:
-                index = voc.index(word)
-                if temp.has_key(index):
-                    temp[index] = temp[index] + 1
-                else:
-                    temp[index] = 1
-            except ValueError:
-                pass
+        try:
+            index = voc.index(word) + 1
+            if temp.has_key(index):
+                temp[index] = temp[index] + 1
+            else:
+                temp[index] = 1
+        except ValueError:
+            pass
     line = ["[ %d " % len(temp),]
     for word,freq in temp.items():
         line.append("%s:%s " %(word,freq))
     line.append("]")
-    print(line)
+    LDAC.append("".join(line))
