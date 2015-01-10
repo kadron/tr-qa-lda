ba58e6f72ddbeea307e11e5fca8ffe6950620ca7
Çağıl Uluşahin <cagilulusahin@gmail.com>
2015-01-10 19:48:20 +0200

    question to ldac added

--- lda/parse.py
+++ lda/parse.py
@@ -1,4 +1,4 @@
-import codecs,pickle
+import codecs,pickle,traceback,sys
 from nltk.corpus import stopwords
 import nltk.data
 
@@ -17,10 +17,21 @@ VOCABULARY = {}
 VOCABULARY_l = []
 LDAC= []
 
-def main():
+class vocabulary(object):
+    def __init__(self,filename):
+        self.vocabulary = {}
+        self.voc_as_l = []
+        if filename:
+            self.voc_as_l = read_voc(filename)
+
+
+def build_vocabulary():
     wikifile = codecs.open("allDocsTogether","rU","utf-8")
-    for _ in range(0,COUNTER):
+    #    for _ in range(0,COUNTER):
+    while 1:
         title = wikifile.readline()
+        if not title:
+            break
         print("Title: %s" %title)
         wikifile.readline()
         body = wikifile.readline()
@@ -62,20 +73,46 @@ def to_ldac():
         wikifile.readline()
         body = wikifile.readline()
         while body != u'\n':
-            from_par_to_ldac(body,voc)
+            from_par_to_ldac(body)
             body = wikifile.readline()
     wikifile.close()
     out = open("corpus1.pickle","wb")
     pickle.dump(VOCABULARY,out)
     out.close()
 
-def from_par_to_ldac(text,voc):
+def from_par_to_ldac(text):
     sentences = tokenizer.tokenize(text)
     for sent in sentences:
         words = word_tokenize(sent)
-        from_sen_to_ldac(words,voc)
+        from_sen_to_ldac(words,VOCABULARY_l,LDAC)
 
-def from_sen_to_ldac(words,voc):
+def read_voc(filename):
+    with codecs.open(filename,"rU","utf-8") as voc_file:
+        voc = voc_file.read().split('\n')
+    return voc
+
+def temp():
+    while True:
+        line = file.readline()
+        if not line:
+            break
+
+def write_questions(filename,voc,ldac_q): # ../data/questions.ldac
+    questions = []
+    with codecs.open("trainingques.csv","rU","utf-8") as ques_file:
+        while True:
+            line = ques_file.readline()
+            if not line:
+                break
+            question = line.split("\t")[1]
+            words = word_tokenize(question)
+            from_sen_to_ldac(words,voc,ldac_q)
+            questions.append(question)
+    with codecs.open(filename,'w+', 'utf-8') as v_file:
+        v_file.write("\n".join(ldac_q))
+    return questions
+
+def from_sen_to_ldac(words,voc,ldac):
     temp = {}
     for word in words:
         if not word.isalnum():
@@ -84,7 +121,7 @@ def from_sen_to_ldac(words,voc):
             except IndexError:
                 continue
         try:
-            index = VOCABULARY_l.index(word) + 1
+            index = voc.index(word)
             if temp.has_key(index):
                 temp[index] = temp[index] + 1
             else:
@@ -94,8 +131,7 @@ def from_sen_to_ldac(words,voc):
     if len(temp) == 0:
         print(words)
         return
-    line = ["[ %d " % len(temp),]
+    line = ["%s " %len(temp),]
     for word,freq in temp.items():
         line.append("%s:%s " %(word,freq))
-    line.append("]")
-    LDAC.append("".join(line))
+    ldac.append("".join(line))
