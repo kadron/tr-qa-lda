97a84821386263bb4ef6be3ec15afe4ba5c53340
Çağıl Uluşahin <cagilulusahin@gmail.com>
2015-01-13 01:05:46 +0200

    find_related all done

--- lda/search_find.py
+++ lda/search_find.py
@@ -14,8 +14,7 @@ def prepare_param_files():
             queryBuilder.buildIndriQuerySingleFromQuestion("queries/tr/lda_"+str(index), line.strip("\n"))
             index += 1
 
-def find_related_docs():
-    index = 1
+def find_related_docs(index):
     query_dir = "/home/cagil/tr-qa-lda/data/"
     doc_ids = indriHandler.singleIndriQuery("tr/lda_"+str(index))
     doc_dir = os.path.join(query_dir, "docs")
@@ -25,3 +24,7 @@ def find_related_docs():
         doc_decoded = doc.decode('utf-8')
         with codecs.open(os.path.join(doc_dir, "lda_"+str(index)+"_"+str(ind)),'w+', 'utf-8') as q_file:
             q_file.write(doc_decoded)
+
+def find_related_docs_all(n=range(1,357)):
+    for index in n:
+        find_related_docs(index)
