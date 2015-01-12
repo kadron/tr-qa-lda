594ea61b0177023de5e27cc1f2bf5d4027cb8061
Çağıl Uluşahin <cagilulusahin@gmail.com>
2015-01-13 01:48:51 +0200

    merge answers

--- lda/search_find.py
+++ lda/search_find.py
@@ -1,6 +1,7 @@
 import os, sys, codecs
 sys.path.append("/Users/cagil/work/HazirCevap/hazircevap2")
 from search import indriDocFetch, indriHandler, queryBuilder
+from nltk import word_tokenize, sent_tokenize
 
 def prepare_param_files():
     index = 1
@@ -28,3 +29,34 @@ def find_related_docs(index):
 def find_related_docs_all(n=range(1,357)):
     for index in n:
         find_related_docs(index)
+
+def merge_and_write_answers_inner(index,answer):
+    for doc_id in range(0,5):
+        all_sentences = []
+        doc_filename = "../data/docs/lda_"+str(index)+"_"+str(doc_id)
+        if os.path.exists(doc_filename):
+            with codecs.open(doc_filename,"rU","utf-8") as doc_file:
+                line = doc_file.readline()
+                while(line != "\n"):
+                    line = doc_file.readline()
+                while True:
+                    if not line:
+                        break
+                    line = doc_file.readline()
+                    sentences = sent_tokenize(line)
+                    for sent in sentences:
+                        all_sentences.append(sent.strip().strip("."))
+    all_related_text = "\n".join(all_sentences)
+    if all_related_text.find(answer) > -1:
+        with codecs.open("../data/answers/index",'w+', 'utf-8') as docs_file:
+            docs_file.write(all_related_text)
+    else:
+        print(index)
+
+
+
+def merge_and_write_answers(n=range(1,357)):
+    with codecs.open("../data/last_questions_answers.txt","rU","utf-8") as ans_file:
+        for index in n:
+            answer = ans_file.readline()
+            merge_and_write_answers_inner(index,answer.strip())
